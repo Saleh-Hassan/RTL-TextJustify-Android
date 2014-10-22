0a17e5b5aef091ae77f3c3f0a995af8b70df3706
Saleh-Hassan <l104106@lhr.nu.edu.pk>
2014-10-22 05:23:43 -0700

    Update TextJustifyUtils.java

--- src/TextJustifyUtils.java
+++ src/TextJustifyUtils.java
@@ -1,263 +1,225 @@
-package com.example.textjustify;
-
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.view.Gravity;
+import android.graphics.Paint.Align;
+import android.util.AttributeSet;
 import android.widget.TextView;
+import com.drdizzy.utils.TextJustifyUtils;
 
-/*!
- * 
- * TextJustifyUtils.java
- * @author Mathew Kurian
- *  
- * From TextJustify-Android Library v1.0.2
- * https://github.com/bluejamesbond/TextJustify-Android
- *
- * Please report any issues
- * https://github.com/bluejamesbond/TextJustify-Android/issues
- * 
- * Date: 12/13/2013 12:29:16 PM
- * 
- */
 
-public class TextJustifyUtils 
+public class TextViewEx extends TextView
 {
-    private static String HYPHEN_SYMBOL = "-";
-    
-    // Please use run(...) instead
-    public static void justify(TextView textView)
-    {
-        Paint paint = new Paint();
-        
-        String [] blocks;
-        float spaceOffset = 0;
-        float textWrapWidth = 0;
+    private Paint paint = new Paint();
 
-        int spacesToSpread;
-        float wrappedEdgeSpace;
-        String block;
-        String [] lineAsWords;
-        String wrappedLine;
-        String smb = "";
-        Object [] wrappedObj;
-        
-        // Pull widget properties
-        paint.setColor(textView.getCurrentTextColor());
-        paint.setTypeface(textView.getTypeface());
-        paint.setTextSize(textView.getTextSize());
-        
-        textWrapWidth = textView.getWidth();
-        spaceOffset = paint.measureText(" ");
-        blocks = textView.getText().toString().split("((?<=\n)|(?=\n))");       
-                
-        if(textWrapWidth < 20)
+    private String [] blocks;
+    private float spaceOffset = 0;
+    private float horizontalOffset = 0;
+    private float verticalOffset = 0;
+    private float horizontalFontOffset = 0;
+    private float dirtyRegionWidth = 0;
+    private boolean wrapEnabled = false;
+    int left,top,right,bottom=0;
+    private Align _align=Align.RIGHT;
+    private float strecthOffset;
+    private float wrappedEdgeSpace;
+    private String block;
+    private String wrappedLine;
+    private String [] lineAsWords;
+    private Object[] wrappedObj;
+
+    private Bitmap cache = null;
+    private boolean cacheEnabled = false;
+
+    public TextViewEx(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        //set a minimum of left and right padding so that the texts are not too close to the side screen
+        this.setPadding(10, 0, 10, 10);
+    }
+
+    public TextViewEx(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        this.setPadding(10, 0, 10, 10);
+    }
+
+    public TextViewEx(Context context)
+    {
+        super(context);
+        this.setPadding(10, 0, 10, 10);
+    }
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom) {
+        // TODO Auto-generated method stub
+
+        super.setPadding(left+10, top, right+10, bottom+10);
+    }
+
+    @Override
+    public void setDrawingCacheEnabled(boolean cacheEnabled)
+    {
+        this.cacheEnabled = cacheEnabled;
+    }
+
+    public void setText(String st, boolean wrap)
+    {
+        wrapEnabled = wrap;
+        super.setText(st);
+    }
+    public void setTextAlign(Align align)
+    {
+        _align=align;
+    }
+    @SuppressLint("NewApi")
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        // If wrap is disabled then,
+        // request original onDraw
+        if(!wrapEnabled)
         {
+            super.onDraw(canvas);
             return;
         }
-        
-        for(int i = 0; i < blocks.length; i++)
+
+        // Active canas needs to be set
+        // based on cacheEnabled
+        Canvas activeCanvas = null;
+
+        // Set the active canvas based on
+        // whether cache is enabled
+        if (cacheEnabled) {
+
+            if (cache != null)
+            {
+                // Draw to the OS provided canvas
+                // if the cache is not empty
+                canvas.drawBitmap(cache, 0, 0, paint);
+                return;
+            }
+            else
+            {
+                // Create a bitmap and set the activeCanvas
+                // to the one derived from the bitmap
+                cache = Bitmap.createBitmap(getWidth(), getHeight(),
+                        Config.ARGB_4444);
+                activeCanvas = new Canvas(cache);
+            }
+        }
+        else
+        {
+            // Active canvas is the OS
+            // provided canvas
+            activeCanvas = canvas;
+        }
+
+        // Pull widget properties
+        paint.setColor(getCurrentTextColor());
+        paint.setTypeface(getTypeface());
+        paint.setTextSize(getTextSize());
+        paint.setTextAlign(_align);
+        paint.setFlags(Paint.ANTI_ALIAS_FLAG);
+
+        //minus out the paddings pixel
+        dirtyRegionWidth = getWidth()-getPaddingLeft()-getPaddingRight();
+        int maxLines = Integer.MAX_VALUE;
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.JELLY_BEAN){
+            maxLines = getMaxLines();
+        }
+        int lines = 1;
+        blocks = getText().toString().split("((?<=\n)|(?=\n))");
+        verticalOffset = horizontalFontOffset = getLineHeight() - 0.5f; // Temp fix
+        spaceOffset = paint.measureText(" ");
+
+        for(int i = 0; i < blocks.length && lines <= maxLines; i++)
         {
             block = blocks[i];
-            
+            horizontalOffset = 0;
+
             if(block.length() == 0)
             {
                 continue;
-            }           
+            }
             else if(block.equals("\n"))
             {
-                smb += block;
+                verticalOffset += horizontalFontOffset;
                 continue;
             }
-                        
+
             block = block.trim();
-            
-            if(block.length() == 0) continue;
-            
-            wrappedObj = TextJustifyUtils.createWrappedLine(block, paint, spaceOffset, textWrapWidth);
+
+            if(block.length() == 0)
+            {
+                continue;
+            }
+            wrappedObj = TextJustifyUtils.createWrappedLine(block, paint, spaceOffset, dirtyRegionWidth);
+
             wrappedLine = ((String) wrappedObj[0]);
             wrappedEdgeSpace = (Float) wrappedObj[1];
             lineAsWords = wrappedLine.split(" ");
-            spacesToSpread = (int) (wrappedEdgeSpace != Float.MIN_VALUE ? wrappedEdgeSpace/spaceOffset : 0);
-            
-            for(String word : lineAsWords)
-            {               
-                smb += word + " ";
-                
-                if(--spacesToSpread > 0) 
+            strecthOffset = wrappedEdgeSpace != Float.MIN_VALUE ? wrappedEdgeSpace/(lineAsWords.length - 1) : 0;
+            for(int j = 0; j < lineAsWords.length; j++)
+            {
+                String word = lineAsWords[j];
+                if (lines == maxLines && j == lineAsWords.length - 1)
                 {
-                    smb += " ";
+                    activeCanvas.drawText("...", horizontalOffset, verticalOffset, paint);
+
+
                 }
+                else if(j==0){
+                    //if it is the first word of the line, text will be drawn starting from right edge of textview
+                    if (_align==Align.RIGHT)
+                    {
+                        activeCanvas.drawText(word, getWidth()-(getPaddingRight()), verticalOffset, paint);
+                        // add in the paddings to the horizontalOffset
+                        horizontalOffset+=getWidth()-(getPaddingRight());
+                    }
+                    else
+                    {
+                        activeCanvas.drawText(word, getPaddingLeft(), verticalOffset, paint);
+                        horizontalOffset+=getPaddingLeft();
+                    }
+
+                }
+                else
+                {
+                    activeCanvas.drawText(word, horizontalOffset, verticalOffset, paint);
+                }
+                if (_align==Align.RIGHT)
+                    horizontalOffset -=  paint.measureText(word) + spaceOffset + strecthOffset;
+                else
+                    horizontalOffset += paint.measureText(word) + spaceOffset + strecthOffset;
             }
-            
-            smb = smb.trim();
-            
+
+            lines++;
+
             if(blocks[i].length() > 0)
             {
                 blocks[i] = blocks[i].substring(wrappedLine.length());
-                
-                if(blocks[i].length() > 0)
-                {
-                    smb += "\n";
-                }
-                
+                verticalOffset += blocks[i].length() > 0 ? horizontalFontOffset : 0;
                 i--;
             }
         }
-        
-        textView.setGravity(Gravity.LEFT);
-        textView.setText(smb);
-    }
-
-    protected static Object [] createWrappedLine(String block, Paint paint, float spaceOffset, float maxWidth, boolean hyphenate, String syllableSeparator)
-    {
-        float cacheWidth = maxWidth;
-
-        String line = "";
-
-        String[] wordSyllables = new String[0];
-        StringBuilder mStringBuilder;
-        String cleanWord;
-        Integer charCounter = 0;
-
-        wordsLoop:
-        for(String dirtyWord : block.split("\\s"))
+        if (cacheEnabled)
         {
-            if (hyphenate) 
-            {
-                mStringBuilder = new StringBuilder();
-                wordSyllables = dirtyWord.split(syllableSeparator);
-
-                for (String syllable : wordSyllables) 
-                {
-                    mStringBuilder.append(syllable);
-                }
-
-                cleanWord = mStringBuilder.toString();
-            } 
-            else 
-            {
-                cleanWord = dirtyWord;
-            }
-
-            cacheWidth = paint.measureText(cleanWord);
-            maxWidth -= cacheWidth;
-
-            if(maxWidth <= 0) // Full word doesn't fit in the line, try syllables
-            {
-                if (hyphenate) 
-                {
-                    for (int i = wordSyllables.length - 2; i >= 0; i--) 
-                    {
-                        maxWidth += cacheWidth;
-                        mStringBuilder = new StringBuilder();
-
-                        for (int j = 0; j <= i; j++) 
-                        {
-                            mStringBuilder.append(wordSyllables[j]);
-                        }
-
-                        cleanWord = mStringBuilder.toString();
-                        cacheWidth = paint.measureText(cleanWord + HYPHEN_SYMBOL);
-                        maxWidth -= cacheWidth;
-
-                        if (maxWidth > 0) 
-                        {
-                            line += cleanWord + HYPHEN_SYMBOL;
-                            charCounter += cleanWord.length() + i + 1;
-                            break wordsLoop;
-                        }
-                    }
-                }
-
-                return new Object[]{line, maxWidth + cacheWidth + spaceOffset, charCounter};
-            }
-
-            line += cleanWord + " ";
-            maxWidth -= spaceOffset;
-            charCounter += dirtyWord.length() + 1;
+            // Draw the cache onto the OS provided
+            // canvas.
+            canvas.drawBitmap(cache, 0, 0, paint);
         }
-
-        if (block.substring(charCounter - 1).length() == 0)  //End of block
-        {
-            return new Object[] { line.substring(0, line.length() - 1), Float.MIN_VALUE, charCounter - 1 };
-        }
-
-        return new Object[] { line, maxWidth, charCounter };
     }
-    
-    final static String SYSTEM_NEWLINE  = "\n";
-    final static float COMPLEXITY = 5.12f;  //Reducing this will increase efficiency but will decrease effectiveness
-    final static Paint p = new Paint();
-
-    /* @author Mathew Kurian */
-
-    public static void run(final TextView tv, float origWidth) {
-        String s = tv.getText().toString();
-        p.setTypeface(tv.getTypeface());        
-        String [] splits = s.split(SYSTEM_NEWLINE);
-        float width = origWidth - 5;
-        for(int x = 0; x<splits.length;x++)
-            if(p.measureText(splits[x])>width){
-                splits[x] = wrap(splits[x], width, p);
-                String [] microSplits = splits[x].split(SYSTEM_NEWLINE);
-                for(int y = 0; y<microSplits.length-1;y++)
-                    microSplits[y] = justify(removeLast(microSplits[y], " "), width, p);
-                StringBuilder smb_internal = new StringBuilder();
-                for(int z = 0; z<microSplits.length;z++)
-                    smb_internal.append(microSplits[z]+((z+1<microSplits.length) ? SYSTEM_NEWLINE : ""));
-                splits[x] = smb_internal.toString();
-            }       
-        final StringBuilder smb = new StringBuilder();
-        for(String cleaned : splits)
-            smb.append(cleaned+SYSTEM_NEWLINE);
-        tv.setGravity(Gravity.LEFT);
-        tv.setText(smb);
-    }
-    private static String wrap(String s, float width, Paint p){
-        String [] str = s.split("\\s"); //regex
-        StringBuilder smb = new StringBuilder(); //save memory
-        smb.append(SYSTEM_NEWLINE);
-        for(int x = 0; x<str.length; x++){
-            float length = p.measureText(str[x]);
-            String [] pieces = smb.toString().split(SYSTEM_NEWLINE);
-            try{
-                if(p.measureText(pieces[pieces.length-1])+length>width)         
-                    smb.append(SYSTEM_NEWLINE);
-            }catch(Exception e){}
-            smb.append(str[x] + " ");
+    public static String reverseWords2(String sentence) {
+        StringBuilder sb = new StringBuilder(sentence.length() + 1);
+        String[] words = sentence.split(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]).append(' ');
         }
-        return smb.toString().replaceFirst(SYSTEM_NEWLINE, "");
-    }
-    private static String removeLast(String s, String g){
-        if(s.contains(g)){
-            int index = s.lastIndexOf(g);
-            int indexEnd = index + g.length();
-            if(index == 0) return s.substring(1);
-            else if(index == s.length()-1)  return s.substring(0, index);
-            else
-                return s.substring(0, index) + s.substring(indexEnd);
-        }
-        return s;
-    }
-    private static String justifyOperation(String s, float width, Paint p){
-        float holder = (float) (COMPLEXITY*Math.random());
-        while(s.contains(Float.toString(holder)))
-            holder = (float) (COMPLEXITY*Math.random());
-        String holder_string = Float.toString(holder);
-        float lessThan = width;
-        int timeOut = 100;
-        int current = 0;
-        while(p.measureText(s)<lessThan&&current<timeOut) {
-            s = s.replaceFirst(" ([^"+holder_string+"])", " "+holder_string+"$1");
-            lessThan = p.measureText(holder_string)+lessThan-p.measureText(" ");
-            current++;          
-        }
-        String cleaned = s.replaceAll(holder_string, " ");
-        return cleaned;
-    }
-    private static String justify(String s, float width, Paint p){
-        while(p.measureText(s)<width){
-            s = justifyOperation(s,width, p);
-        }
-        return s;
+        sb.setLength(sb.length() - 1);  // Strip trailing space
+        return sb.toString();
     }
 }
